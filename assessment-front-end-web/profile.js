15d1a25bc32553f5458a0e79d10f603810e67284
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-02 12:00:53 -0600

    fifth

---
+++ assessment-front-end-web/profile.js
@@ -0,0 +1,23 @@
+let colorButton = document.querySelector('#color')
+let placeButton = document.querySelector('#place')
+let ritualButton = document.querySelector('#ritual')
+
+
+const colorFunction = (event) => {
+    event.preventDefault();
+	alert('Green is my favorite color');
+}
+
+const placeFunction = (event) => {
+    event.preventDefault();
+	alert('Making Espresso is my favorite ritual');
+}
+
+const ritualFunction = (event) => {
+    event.preventDefault();
+	alert('Seattle is my favorite place');
+}
+
+colorButton.addEventListener('click', colorFunction)
+placeButton.addEventListener('click', placeFunction)
+ritualButton.addEventListener('click', ritualFunction)
